944b75c563400fdd0cfaf7212e6bda7304ae66ea
Deannv <afilfriend@gmail.com>
2025-08-11 09:04:46 +0800

    Initial Commit

---
+++ MyLibraryApp/MyLibraryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryAppApp.swift
+//  MyLibraryApp
+//
+//  Created by Kemas Deanova on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyLibraryAppTests/MyLibraryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryAppTests.swift
+//  MyLibraryAppTests
+//
+//  Created by Kemas Deanova on 11/08/25.
+//
+
+import Testing
+@testable import MyLibraryApp
+
+struct MyLibraryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
